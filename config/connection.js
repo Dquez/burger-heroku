e0b51ff0591a0928288939cac4a0243f976f3b23
Dariell Vasquez <dariellv@yahoo.com>
2019-01-19 14:39:46 -0500

    heroku creds

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,7 @@ var mysql = require("mysql");
 var connection;
 if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
-else {
+} else {
     connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -12,7 +11,6 @@ else {
     database: "burgers_db"
   });
 };
-
 
 
 connection.connect(function(err) {
